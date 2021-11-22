20055a809c8828b0d924bd4def3a2f473991c7bb
aldnr26 <aldnr26@gmail.com>
2021-11-22 15:08:43 +0700

    contoh tambah file

---
+++ Eps21.py
@@ -0,0 +1,35 @@
+# if dan else statement
+
+"""
+1. if nya
+2. kondisinya
+3. aksinya
+"""
+
+nama = input("Nama saya adalah = ")
+
+#program sebelumnya
+#if kondisi: aksi
+# program selanjutnya
+
+"""1. Program if inline
+if nama == "ucup" : print("kamu ganteng abiezzz!!!")
+print(f"Terima kasih {nama}")
+"""
+# 2. program if indentation
+"""
+if nama == "ucup":
+    print("Kamu Ganteng ABiezzzz!!!")
+    print("Kamu juga keren")
+print(f"Terima Kasih {nama}")"""
+
+#3. Else Statement
+
+if nama == "aldrian":
+    print("Kamu Ganteng ABiezzzz!!!")
+    print("Kamu pacarnya dinda ya?")
+else:
+    print("kamu bukan ALDRIAN, Kamu ga ganteng!!!!")
+
+print(f"Terima Kasih {nama}")
+
